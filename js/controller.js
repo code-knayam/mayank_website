6038f1ecbe601e4dd6b245322ea3d9a5958af033
code-knayam <mayankjain1394@gmail.com>
2017-10-03 11:29:09 +0530

    Updated code to open in new tab.

--- js/controller.js
+++ js/controller.js
@@ -6,14 +6,16 @@ app.controller('worksController', function($scope) {
       'image_path' : 'images/dams.png',
       'type' : 'Website',
       'tags' : 'HTML, CSS, jQuery, PHP, AJAX, MySQL',
-      'link' : 'http://www.damskanpur.org/beta'
+      'link' : 'http://www.damskanpur.org/beta',
+      'tab' : '_blank'
     },
     {
       'title' : 'Bhavna - NGO',
       'image_path' : 'images/ngo_bhavna.png',
       'type' : 'Website',
       'tags' : 'HTML, CSS, jQuery, PHP, AJAX, MySQL',
-      link : "http://www.bhavna.org.in"
+      link : "http://www.bhavna.org.in",
+      'tab' : '_blank'
     }
   ];
 
@@ -27,47 +29,54 @@ app.controller('projectsController', function($scope) {
       'image_path' : 'images/p5_logo.png',
       'type' : 'P5 JS',
       'tags' : 'p5.js, HTML',
-      'link' : 'https://code-knayam.github.io/p5-js/'
+      'link' : 'p5-js/',
+      'tab' : ''
     },
     {
       'title' : 'Facebook Birthday Wish',
       'image_path' : 'images/fb_logo.png',
       'type' : 'Python App',
       'tags' : 'Python, Facebook Graph API',
-      'link' : 'https://github.com/code-knayam/birthdayWishApp'
+      'link' : 'https://github.com/code-knayam/birthdayWishApp',
+      'tab' : '_blank'
     },
     {
       'title' : 'Gym-A-Log',
       'image_path' : 'images/gymAlog.png',
       'type' : 'Website',
       'tags' : 'HTML, CSS, jQuery, PHP, AJAX, MySQL',
-      'link' : 'http://www.mayankjain.me/gymAlog'
+      'link' : 'http://www.mayankjain.me/gymAlog',
+      'tab' : '_blank'
     },
     {
       'title' : 'Chatter',
       'image_path' : 'images/chatter.png',
       'type' : 'App',
       'tags' : 'HTML, CSS, jQuery, NodeJS',
-      'link' : 'https://github.com/code-knayam/Chatter'
+      'link' : 'https://github.com/code-knayam/Chatter',
+      'tab' : '_blank'
     },
     {
       'title' : 'Weather Emailer',
       'image_path' : 'images/weatherEmailer.png',
       'type' : 'Pyhton App',
       'tags' : 'Python',
-      'link' : 'https://github.com/code-knayam/weather_emailer'
+      'link' : 'https://github.com/code-knayam/weather_emailer',
+      'tab' : '_blank'
     },{
       'title' : 'parallax Gallery',
       'image_path' : 'images/parallaxGallery.png',
       'type' : 'Concept',
       'tags' : 'HTML, CSS, jQuery',
-      'link' : 'http://www.mayankjain.me/parallaxgallery'
+      'link' : 'http://www.mayankjain.me/parallaxgallery',
+      'tab' : '_blank'
     },{
       'title' : 'Cubify',
       'image_path' : 'images/cubify.jpeg',
       'type' : 'Android App',
       'tags' : 'OpenGl',
-      'link' : 'cubify.apk'
+      'link' : 'cubify.apk',
+      'tab' : '_blank'
     },
   ];
 
